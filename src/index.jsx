6ac36b1daefa4fa6acd6d7063204483eb03428ff
Martina Vokáčová <martinavokacova@gmail.com>
2023-03-20 16:41:44 +0100

    color-box

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
-import ColorSlider from './components/ColorSlider';
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import ColorSlider from "./components/ColorSlider";
 
 const App = () => {
-
+  const [redValue, setRedValue] = useState("");
+  const [greenValue, setGreenValue] = useState("");
+  const [blueValue, setBlueValue] = useState("");
 
   return (
-
-	<div class="color-panel">
-		<h1>Mixér barev</h1>
-		<div class="sliders">
-
-    <ColorSlider colorName='červená' baseColor='red' />
-    <ColorSlider colorName='zelená' baseColor='green' />
-    <ColorSlider colorName='modrá' baseColor='blue' />
-
-		</div>
-		<div class="color-box" id="color-box"></div>
-	</div>
+    <div class="color-panel">
+      <h1>Mixér barev</h1>
+      <div class="sliders">
+        <ColorSlider colorName="červená" baseColor="red" />
+        <ColorSlider colorName="zelená" baseColor="green" />
+        <ColorSlider colorName="modrá" baseColor="blue" />
+      </div>
+      <div
+        class="color-box"
+        id="color-box"
+        style={{
+          backgroundColor: `rgb(${redValue}, ${greenValue}, ${blueValue}`,
+        }}
+      ></div>
+    </div>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector("#app")).render(<App />);
